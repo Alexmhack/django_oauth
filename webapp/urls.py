2083c3f31459ec9aa09230322760b2428a0013ed
alexHooper <alexmohack@gmail.com>
2018-09-24 17:45:11 +0530

    add project urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import login, logout
+
+from display.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home')
+]
+
+urlpatterns += [
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+]
+
+urlpatterns += [
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
