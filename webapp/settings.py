f6f5fc631366f088857090c486f6746e69a702ed
alexHooper <alexmohack@gmail.com>
2018-09-24 17:16:56 +0530

    add middleware for social_django

--- webapp/settings.py
+++ webapp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'webapp.urls'
