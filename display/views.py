93b1c3aafe227738268fc4d2bb62148ee0510f3d
alexHooper <alexmohack@gmail.com>
2018-09-24 18:42:57 +0530

    create dashboard view

--- display/views.py
+++ display/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home_view(request):
 	return render(request, 'index.html')
+
+
+@login_required
+def dashboard_view(request):
+	return render(request, 'dashboard.html')
